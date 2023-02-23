c7dbdf4506e99e29b3f48f8eaa50dd898a8eecb0
KedarKK1 <officialkedark1@gmail.com>
2023-02-23 12:18:22 +0530

    solved 'document is not found' error in setup of tw-element, found error in _documents.js, set up scripts there

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-// import 'tw-elements';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div >
       <Head>
-        {/* <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script> */}
       </Head>
         +Hello World
     </div>
