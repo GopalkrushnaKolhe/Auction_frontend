4b7d2efb3408cf8528efd4ae7c068b63673e7e51
Sarthak <sarthakhupare25@gmail.com>
2023-04-27 09:48:24 +0530

    Auction Calender Cards done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
 // import '@/styles/globals.css' // not working, dont know why! Used below line as solution
 import '../styles/globals.css';
 import { useEffect } from "react";
+// import Navbar from './Components/Navbar';
 
 export default function App({ Component, pageProps }) {
   // return <Component {...pageProps} /> // changed this to enable tailwind css and tailwind element
+
   useEffect(() => {
+
+    // <Navbar/>
+
     const use = async () => {
       (await import('tw-elements')).default;
     };
